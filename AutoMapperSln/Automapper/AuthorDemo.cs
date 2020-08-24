1b5856e95316d27865efc7d43d47d59b075145cc
kn52 <ashish52922@gmail.com>
2020-08-24 09:49:03 +0530

    [Aashish]ADDED:Automapper to convert one class object to another class

---
+++ AutoMapperSln/Automapper/AuthorDemo.cs
@@ -0,0 +1,22 @@
+﻿namespace AutoMapperSln.Automapper
+{
+    using AutoMapper;
+    using System;
+    public class AuthorDemo
+    {
+        public static void AMapper()
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Author, AuthorDTO>(); });
+            IMapper iMapper = config.CreateMapper();
+            var author = new Author
+            {
+                Id = 1,
+                FirstName = "Stephen",
+                LastName = "Hawking",
+                Address = "United Kingdom"
+            };
+            var authorDTO = iMapper.Map<Author, AuthorDTO>(author);
+            Console.WriteLine("Author Name: " + authorDTO.FirstName + " " + authorDTO.LastName);
+        }    
+    }
+}
---
+++ AutoMapperSln/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AutoMapperSln
+{
+    using AutoMapper;
+    using AutoMapperSln.Automapper;
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().AutoMapperDemo();
+            Console.ReadKey();
+        }
+
+        private void AutoMapperDemo()
+        {
+            while (choice.Equals("y"))
+            {
+                Console.WriteLine("=====Auto Mapper=====");
+                Console.WriteLine("1. Auto Mapper");
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AuthorDemo.AMapper();
+                        break;
+                }
+            }
+        }
+
+        private string choice = "y";
+    }
+}
